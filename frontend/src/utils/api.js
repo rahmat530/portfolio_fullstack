1c947a4dad24f42c308ffdbea85c9982887ebbad
rahmat530 <rahmatullah.wahdat046@gmai.com>
2026-06-07 18:26:42 +0200

    Update API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const BASE_URL =
+  import.meta.env.VITE_API_URL || "https://portfolio-backend-annb.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
